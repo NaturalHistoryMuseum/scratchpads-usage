a44e9fd0ce061c7237bbece4f59293d1fcd8b8fb
Paul Kiddle <p.kiddle@nhm.ac.uk>
2021-02-01 09:39:51 +0000

    Add totals to recently edited sites list

--- scripts/reports/recently-edited/data.js
+++ scripts/reports/recently-edited/data.js
@@ -1,10 +1,19 @@
 // Get a list of the most recently edited sites
 
-const recentNodes = sql => sql`select site, changed from recent_changednodes`;
-const recentTaxon = sql => sql`select site, timestamp as changed from recent_taxonomy`;
+const recentNodes = sql => sql`select site,	changed,	TRUE as isNode	from recent_changednodes`;
+const recentTaxon = sql => sql`select site,	timestamp as changed,	NULL as isNode	from recent_taxonomy`;
 
 export default async function getRecent(sql) {
-	const sites =	await sql`select site, max(changed) as changed from (${recentNodes} union ${recentTaxon}) group by site order by changed desc`
+	const sites = await sql`
+		SELECT
+			site,
+			max(changed) AS changed,
+			COUNT(isNode) AS nodes,
+			COUNT(*)-COUNT(isNode) AS taxa,
+			COUNT(*) as total
+		FROM (${recentNodes} UNION ${recentTaxon})
+		GROUP BY site
+		ORDER BY changed DESC`
 
 	for(const site of sites) {
 		site.changed = new Date(parseInt(site.changed, 10)*1000);
--- scripts/reports/recently-edited/view.js
+++ scripts/reports/recently-edited/view.js
@@ -3,13 +3,16 @@ import html from 'encode-html-template-tag';
 /**
  * Template for most recently used sites table
  */
-export default data => html`
+export default (data) => html`
+<p>A list of all Scratchpads for which a node or taxonomy term has been edited in the three months up to ${data[0].changed.toLocaleDateString()}.</p>
+<p>This list contains ${data.length} sites.</p>
 <table>
 	<thead>
-		<tr><th>Site</th><th>Changed</th></tr>
+		<tr><th rowspan=2>Site</th><th rowspan=2>Changed</th>	<th colspan=3>Number of changed items</th></tr>
+		<tr>	<th>Nodes</th><th>Taxa</th><th>Total</th></tr>
 	</thead>
 	<tbody>${data.map(row=>html`
-		<tr><td>${row.site}</td><td>${row.changed.toLocaleDateString()}</td></tr>`)}
+		<tr><td>${row.site}</td><td>${row.changed.toLocaleDateString()}</td><td>${row.nodes}</td><td>${row.taxa}</td><td>${row.total}</td></tr>`)}
 	</tbody>
 </table>
 `;
