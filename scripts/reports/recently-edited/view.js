6c67bb8b79d1ff2091336e29ee5afec6903c8c15
Paul Kiddle <p.kiddle@nhm.ac.uk>
2021-02-01 10:49:21 +0000

    Align numeric columns

--- scripts/reports/recently-edited/view.js
+++ scripts/reports/recently-edited/view.js
@@ -4,6 +4,11 @@ import html from 'encode-html-template-tag';
  * Template for most recently used sites table
  */
 export default (data) => html`
+<style>
+.numeric {
+	text-align:right;
+}
+</style>
 <p>A list of all Scratchpads for which a node or taxonomy term has been edited in the three months up to ${data[0].changed.toLocaleDateString()}.</p>
 <p>This list contains ${data.length} sites.</p>
 <table>
@@ -12,7 +17,11 @@ export default (data) => html`
 		<tr>	<th>Nodes</th><th>Taxa</th><th>Total</th></tr>
 	</thead>
 	<tbody>${data.map(row=>html`
-		<tr><td>${row.site}</td><td>${row.changed.toLocaleDateString()}</td><td>${row.nodes}</td><td>${row.taxa}</td><td>${row.total}</td></tr>`)}
+		<tr><td>${row.site}</td>
+		<td>${row.changed.toLocaleDateString()}</td>
+		<td class="numeric">${row.nodes}</td>
+		<td class="numeric">${row.taxa}</td>
+		<td class="numeric">${row.total}</td></tr>`)}
 	</tbody>
 </table>
 `;
