9b2f1c3c0dbdd051a077c8eea0e57fad39ee23e1
Paul Kiddle <p.kiddle@nhm.ac.uk>
2021-05-06 17:06:27 +0100

    Add missing sites to site list

--- reports/pages/site-list/data.js
+++ reports/pages/site-list/data.js
@@ -1,13 +1,16 @@
 export default function(sql){
 	return sql`
 		select
-			nodes.site,
-			nodes.count as nodes,
-			taxonomy.terms,
-			taxonomy.vocabs
+			system.site as name,
+			ifnull(nodes.count, 0) as nodes,
+			ifnull(taxonomy.terms, 0) as terms,
+			ifnull(taxonomy.vocabs, 0) as vocabs
 		from
-			(select site, sum(count) as count from nodes group by site) as nodes
+			(select site from system group by site) as system
+			left join
+				(select site, sum(count) as count from nodes group by site) as nodes
+				on (system.site = nodes.site)
 			left join
 				(select site, sum(total) as terms, count(distinct vid) as vocabs from taxonomy_terms group by site) as taxonomy
-				on (nodes.site=taxonomy.site);`
+				on (system.site=taxonomy.site);`
 }
--- reports/pages/site-list/view.js
+++ reports/pages/site-list/view.js
@@ -1,10 +1,16 @@
 import html from 'encode-html-template-tag';
 import {table, numeric} from 'sp-templates';
 
-export default (data) => html`
+const json = html`
+<textarea>[
+${data.map(d=>JSON.stringify(d)).join(',\n')}
+]</textarea>`;
 
+export default (data) => html`
 	${table(
-		['Site', numeric('Nodes'), numeric('Taxonomy Terms', 'terms'), numeric('Taxonomy Vocabularies', 'vocabs')],
+		[['Site', 'name'], numeric('Nodes'), numeric('Taxonomy Terms', 'terms'), numeric('Taxonomy Vocabularies', 'vocabs')],
 		data
 	)}
+
+	${json}
 `;
